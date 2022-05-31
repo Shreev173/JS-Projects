f43a690c704749b8fde4b8f1fb2e3b9f9c61c2c7
shreeshan <shreev173@gmail.com>
2022-05-31 21:29:45 +0545

    clock

---
+++ Day 1/script.js
@@ -0,0 +1,18 @@
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+const deg = 6;
+
+//calls a function at specified intervals
+setInterval(() => {
+  let day = new Date();
+  let ms = day.getMilliseconds() * deg;
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg + ms / 1000;
+
+  hr.style.transform = `rotateZ(${hh}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
